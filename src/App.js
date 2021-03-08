0426007dcacb63ff62de9d20fd4c25d3950f1534
Michael Huntington <2466745+MikeHuntington@users.noreply.github.com>
2021-03-08 00:29:07 -0500

    removing validation

--- src/App.js
+++ src/App.js
@@ -9,12 +9,6 @@ function App() {
   const handleSubmit = async (event) => {
     if (typeof email !== "undefined") {
 
-      var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-    
-      if (!pattern.test(email)) {
-        alert("Invalid email entered");
-        return;
-      }
 
       axios.post(`http://gamepower.network:3000/email/`, {email})
       .then(res => {
